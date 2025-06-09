cdf33a34d95d5d21ae120c870903fb2b2e2d4c87
Rogan <jh241093@gmail.com>
2025-06-09 16:45:49 +0900

    fix: 존재하지 않는 함수 삭제

--- main.js
+++ main.js
@@ -33,9 +33,11 @@ function setBounds() {
     // LatLngBounds 객체에 추가된 좌표들을 기준으로 지도의 범위를 재설정합니다
     // 이때 지도의 중심좌표와 레벨이 변경될 수 있습니다
     map.setBounds(bounds);
-    map.setZoom(3);
+
+    // 존재하지 않는 함수
+    // map.setZoom(3);
   } catch (error) {
-    console.log(">>>>> error", error);
+    console.log("Error setBounds", error);
     // 에러 발생 시 Flutter에 전송
     window.mapError.postMessage(`${error.message} \n bounds: ${bounds}`);
   }
