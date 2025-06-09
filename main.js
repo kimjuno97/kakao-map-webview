c0f1b83f26f66eb2339dec40084fb00315ff760a
Rogan <jh241093@gmail.com>
2025-06-09 15:47:28 +0900

    test: 좌표 돌아가기 추가

---
+++ main.js
@@ -0,0 +1,30 @@
+const params = new URLSearchParams(window.location.search);
+const appKey = params.get("appKey");
+const lat = params.get("lat");
+const lng = params.get("lng");
+
+const script = document.createElement("script");
+script.type = "text/javascript";
+script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${appKey}&autoload=false&libraries=clusterer,services`;
+document.head.appendChild(script);
+
+script.onload = () => {
+  kakao.maps.load(() => {
+    /** 지도 */
+    const mapContainer = document.getElementById("map");
+
+    const mapOption = {
+      center: new kakao.maps.LatLng(lat, lng),
+      level: 3,
+    };
+
+    const map = new kakao.maps.Map(mapContainer, mapOption);
+    const bounds = new kakao.maps.LatLngBounds();
+
+    window.setBounds = function () {
+      // LatLngBounds 객체에 추가된 좌표들을 기준으로 지도의 범위를 재설정합니다
+      // 이때 지도의 중심좌표와 레벨이 변경될 수 있습니다
+      map.setBounds(bounds);
+    };
+  });
+};
