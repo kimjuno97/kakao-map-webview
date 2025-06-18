24f30543fb6169025b0a9faeee9a266de6515371
rogan <jh241093@gmail.com>
2025-06-18 11:38:42 +0900

    refactor: id변수 비교 오류 수정

--- main.js
+++ main.js
@@ -138,25 +138,12 @@ function renderOverlay({ storeName, storeId, isOn, isMain, position }) {
           const storeEl = findStoreEls[i];
           // [MEMO] storeEl.id가 string이고 storeId가 number일 수 있음
           const isMain = storeEl.id == storeId;
-          console.log(
-            ">>>>>> isMain",
-            isMain,
-            "storeId",
-            storeId,
-            "storeEl.id",
-            storeEl.id,
-            "typeof storeEl.id",
-            typeof storeEl.id,
-
-            "typeof storeId",
-            typeof storeId
-          );
 
           const storeNameEl = storeEl.querySelector(".storeName");
           const storeImgEl = storeEl.querySelector(".storeImg");
 
           /// storeNameEl이 존재하는지 확인
-          if (storeEl.id === selectedStoreId) {
+          if (storeEl.id == selectedStoreId) {
             storeNameEl.style.backgroundColor = "#d2ff53";
             storeImgEl.src = isMain ? mainMarkerOnSrc : subMarkerOnSrc;
           } else {
